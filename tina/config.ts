88f00342f890b521df3e327c7ed5a55e089d991a
Daniel Xu <64868985+x0ba@users.noreply.github.com>
2025-07-24 18:03:19 +0800

    add some more cms features

--- tina/config.ts
+++ tina/config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
-    collections: [Post, Page],
+    collections: [Post],
   },
   search: {
     tina: {
